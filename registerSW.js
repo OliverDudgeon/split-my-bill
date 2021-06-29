7778bdadfde92cfc35ec788a6d1d27cb6c658b58
OliverDudgeon <OliverDudgeon@users.noreply.github.com>
2021-06-29 21:54:43 +0000

    deploy: bedb93e3a18e67d76ab0565e13a6d8ee624b2fc5

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/split-my-bill/sw.js', { scope: '/split-my-bill/' })})}
